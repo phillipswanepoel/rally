d00a7a183a8cab7d70d8caacd2929ea08aa7b037
phillip_personal <phillips@Phillips-MacBook-Pro.local>
2026-06-14 17:03:44 +0200

    spawn

---
+++ rallyline.gd
@@ -0,0 +1,21 @@
+extends Line2D
+class_name RallyLine
+
+signal rally_set(pos: Vector2)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("MouseLeft"):
+		rally_set.emit(get_global_mouse_position())
+		queue_free()
+
+
+func _process(_delta: float) -> void:
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	points[-1] = mouse_pos
+
+
+func _init(start_pos: Vector2) -> void:
+	var p: PackedVector2Array = PackedVector2Array()
+	p.push_back(start_pos)
+	p.push_back(start_pos)
+	points = p
--- spawner.gd
+++ spawner.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+# TODO: minimum bound from spawner size/rect
+func _on_button_pressed() -> void:
+	create_rally_line()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func create_rally_line() -> void:
+	var rally_line = RallyLine.new(position)
+	rally_line.rally_set.connect(_on_rally_set)
+	add_child(rally_line)
 
+@onready var rally_marker := $RallyMarker
+@onready var rally_path := $RallyPath
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_rally_set(p: Vector2) -> void:
+	rally_marker.position = p
+	rally_marker.visible = true
+
+	var c := Curve2D.new()
+	c.add_point(position)
+	c.add_point(p)
+	rally_path.curve = c
